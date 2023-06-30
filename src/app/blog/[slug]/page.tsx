82ef5ad093b8bbf3e4f7765063887e1e8ee1f203
ThePianist <cafarimo@outlook.com>
2023-06-30 03:47:27 -0400

    Add static params for posts static generation

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,6 +8,11 @@ interface Props {
   params: { slug: string };
 }
 
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(res => res.json());
+  return posts.map(post => ({ slug: post.slug }));
+}
+
 export default async function BlogPostPage({ params }: Props) {
   const posts: Post[] = await fetch("http://localhost:3000/api/content").then(res => res.json());
   const post = posts.find(post => post.slug === params.slug)!;

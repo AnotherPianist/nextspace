1afb61b054e02c19d4ae09b82c0cac7e700f67d8
ThePianist <cafarimo@outlook.com>
2023-06-30 23:04:13 -0400

    Fetch and show users

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,24 @@
+import { inspect } from "util";
+import styles from "./UserCard.module.css";
+import Link from "next/link";
+
+interface Props {
+  id: string;
+  name: string | null;
+  age: number | null;
+  image: string | null;
+}
+
+export default function UserCard({ id, name, age, image }: Props) {
+  return (
+    <div className={styles.card}>
+      <img src={image ?? "/mememan.webp"} alt={`${name}'s profile`} className={styles.cardImage} />
+      <div className={styles.cardContent}>
+        <h3>
+          <Link href={`/users/${id}`}>{name}</Link>
+        </h3>
+        <p>Age: {age}</p>
+      </div>
+    </div>
+  );
+}
